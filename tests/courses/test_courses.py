abfab6542ceb2b3e4e69231e4be467f65dcf8e67
Ozondo <joker1catpro@gmail.com>
2025-12-05 17:58:54 +0300

    HOMEWORK 7.7

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -53,3 +53,47 @@ class TestCourses:
 
         course_list_page.toolbar_view.check_visible()
         course_list_page.check_visible_empty_view()
+
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, course_list_page: CoursesListPage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+        create_course_page.create_course_form.fill(
+            title='Playwright',
+            estimated_time='2 weeks',
+            description='Playwright',
+            max_score='100',
+            min_score='10',
+        )
+
+        create_course_page.image_upload_widget.upload_preview_image(file='test_data/files/image.png')
+
+        create_course_page.click_create_course_button()
+
+        course_list_page.course_view.check_visible(
+            index=0,
+            title='Playwright',
+            estimated_time='2 weeks',
+            max_score='100',
+            min_score='10',
+        )
+
+        course_list_page.course_view.menu.click_edit(index=0)
+
+        create_course_page.create_course_form.fill(
+            title='Python',
+            estimated_time='1 hour',
+            description='Python',
+            max_score='10',
+            min_score='1',
+        )
+
+        create_course_page.click_create_course_button()
+
+        course_list_page.course_view.check_visible(
+            index=0,
+            title='Python',
+            estimated_time='1 hour',
+            max_score='10',
+            min_score='1',
+        )
