e155375000de55bebddde5ab846003a90bfe6a89
Ozondo <joker1catpro@gmail.com>
2025-12-09 14:44:01 +0300

    Homework 8.2

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -16,6 +16,9 @@ from allure_commons.types import Severity
 @allure.epic(AllureEpic.LMS)
 @allure.feature(AllureFeature.COURSES)
 @allure.story(AllureStory.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
 class TestCourses:
     @allure.severity(Severity.CRITICAL)
     @allure.title("Create course")
