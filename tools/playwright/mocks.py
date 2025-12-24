bef09f40802a85ea92f0854d2dd5ff9ab2b81b36
Ozondo <joker1catpro@gmail.com>
2025-12-24 19:06:33 +0300

    Моки и оптимизация UI автотестов

--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -2,4 +2,4 @@ from playwright.sync_api import Page, Route
 
 
 def mock_static_resources(page: Page):
-    page.route("**/*.{ico,png,svg,webm,mp3,mp4,woff,woff2}", lambda route: route.abort)
+    page.route("**/*.{ico,png,svg,webm,mp3,mp4,woff,woff2}", lambda route: route.abort())
